44195ec290c06960fbf5a40c085e4f35db0b5937
Theryston Santos <devtheryston@gmail.com>
2025-12-05 15:10:19 -0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,41 @@
+import "dotenv/config";
+
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const token = process.env.TOKEN;
+
+if (!token) {
+  console.error("TOKEN is not set");
+  process.exit(1);
+}
+
+const tokenAuth = new Elysia({ name: "tokenAuth" }).macro({
+  tokenAuth: () => ({
+    beforeHandle({ headers }) {
+      const authorization =
+        headers["authorization"] || headers["Authorization"];
+
+      if (authorization !== `Bearer ${token}`) {
+        throw new Error("Unauthorized");
+      }
+    },
+  }),
+});
+
+const app = new Elysia()
+  .use(tokenAuth)
+  .get(
+    "/health",
+    () => {
+      return {
+        message: "OK",
+      };
+    },
+    {
+      tokenAuth: true,
+    }
+  )
+  .listen(9856);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
